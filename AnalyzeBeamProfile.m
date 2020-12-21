c4391dacd68cc370cd1d6f3e5f38ef28f2d9d1cc
npnelson3 <71889667+npnelson3@users.noreply.github.com>
2020-12-21 16:16:57 -0600

    Add files via upload

--- AnalyzeBeamProfile.m
+++ AnalyzeBeamProfile.m
@@ -1,13 +1,17 @@
 % Inputs are ax = off-axis distance array and prof = prof array
 function [penumbra_l, penumbra_r,FWHM_l, FWHM_r,FS] = AnalyzeBeamprof(x_data, prof)
 prof = prof ./ max(prof);
-idx = find(prof == max(prof(:)));
-i20_l = interp1(prof(1:idx(1)), x_data(1:idx(1)), 0.2);
-i80_l = interp1(prof(1:idx(1)), x_data(1:idx(1)), 0.8);
+% take care of unique things
+[prof,idx]=unique(prof);
+x_data = x_data(idx);
+idx = find(x_data < 0);
+i20_l = interp1(prof(idx), x_data(idx), 0.2);
+i80_l = interp1(prof(idx), x_data(idx), 0.8);
 penumbra_l = abs(i20_l) - abs(i80_l);
 
-i80_r = interp1(prof(idx(1)+1:end), x_data(idx(1)+1:end), 0.8);
-i20_r = interp1(prof(idx(1)+1:end), x_data(idx(1)+1:end), 0.2);
+idx = find(x_data > 0);
+i80_r = interp1(prof(idx), x_data(idx), 0.8);
+i20_r = interp1(prof(idx), x_data(idx), 0.2);
 penumbra_r = i20_r - i80_r;
 
 
